23d354be324882f5e452f848ff02adb7702f070d
FaytMx <silverzero55@gmail.com>
2021-09-05 21:15:03 -0500

    ruta para obtener todos los usuarios

--- server.js
+++ server.js
@@ -1,27 +1,38 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const http = require("http");
+const http = require('http');
 const server = http.createServer(app);
-const logger = require("morgan");
-const cors = require("cors");
+const logger = require('morgan');
+const cors = require('cors');
+
+/**
+ * Rutas
+ */
+
+const users = require('./routes/usersRoutes');
 
 const port = process.env.PORT || 3000;
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.disable("x-powered-by");
+app.disable('x-powered-by');
 
-app.set("port", port);
+app.set('port', port);
 
-server.listen(3000, "192.168.0.35" || "localhost", function () {
-  console.log("Applicacion de NodeJS " + process.pid + " Iniciada...");
+/**
+ * Llamando a las rutas
+ */
+users(app);
+
+server.listen(3000, '192.168.0.35' || 'localhost', function () {
+  console.log('Applicacion de NodeJS ' + process.pid + ' Iniciada...');
 });
 
-app.get("/", (req, res) => {
-  res.send("Ruta raiz");
+app.get('/', (req, res) => {
+  res.send('Ruta raiz');
 });
 
 //ERROR HANDLER
@@ -30,3 +41,8 @@ app.use((err, req, res, next) => {
   console.log(err);
   res.status(err.status || 500).send(err.stack);
 });
+
+module.exports = {
+  app,
+  server,
+};
