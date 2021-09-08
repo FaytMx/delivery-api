14d2a1eb04b30a39783a7d57d7d775c1092e063a
FaytMx <silverzero55@gmail.com>
2021-09-07 21:07:01 -0500

    registro

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -14,4 +14,24 @@ module.exports = {
       });
     }
   },
+
+  async register(req, res, next) {
+    try {
+      const user = req.body;
+      const data = await User.create(user);
+
+      return res.status(201).json({
+        success: true,
+        message: 'El registro se realizo correctamente',
+        data: data.id,
+      });
+    } catch (error) {
+      console.log('Error: ', error);
+      return res.status(501).json({
+        success: false,
+        message: 'Error al registrar el usuario',
+        error: error,
+      });
+    }
+  },
 };
