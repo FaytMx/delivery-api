a39c9e312aa09712d47a0250a2697fb612029e1c
FaytMX <silverzero55@gmail.com>
2021-10-08 22:03:23 -0500

    configurado upload a firebase

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,9 +1,9 @@
 const UsersController = require('../controllers/usersController');
 
-module.exports = (app) => {
+module.exports = (app, upload) => {
   app.get('/api/users/getAll', UsersController.getAll);
 
-  app.post('/api/users/create', UsersController.register);
+  app.post('/api/users/create',upload.array('image',1), UsersController.registerWithImage);
 
   app.post('/api/users/login', UsersController.login);
 };
--- server.js
+++ server.js
@@ -1,38 +1,51 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
-const logger = require('morgan');
-const cors = require('cors');
+const logger = require("morgan");
+const cors = require("cors");
+const multer = require("multer");
+const admin = require("firebase-admin");
+const serviceAcount = require("./serviceAccountKey.json");
 
+/*
+ * Inicializar firebase admin
+ */
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAcount),
+});
+
+const upload = multer({
+  storage: multer.memoryStorage(),
+});
 /**
  * Rutas
  */
 
-const users = require('./routes/usersRoutes');
+const users = require("./routes/usersRoutes");
 
 const port = process.env.PORT || 3000;
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.disable('x-powered-by');
+app.disable("x-powered-by");
 
-app.set('port', port);
+app.set("port", port);
 
 /**
  * Llamando a las rutas
  */
-users(app);
+users(app, upload);
 
-server.listen(3000, '192.168.0.36' || 'localhost', function () {
-  console.log('Applicacion de NodeJS ' + process.pid + ' Iniciada...');
+server.listen(3000, "192.168.0.36" || "localhost", function () {
+  console.log("Applicacion de NodeJS " + process.pid + " Iniciada...");
 });
 
-app.get('/', (req, res) => {
-  res.send('Ruta raiz');
+app.get("/", (req, res) => {
+  res.send("Ruta raiz");
 });
 
 //ERROR HANDLER
